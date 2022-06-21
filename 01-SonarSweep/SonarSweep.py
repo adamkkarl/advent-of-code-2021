7a60ffe2b213c9d198f674e326b40371d38d2d22
adamkkarl <adamkkarl@gmail.com>
2022-06-20 22:01:45 -0400

    day 1 part 1 solved

---
+++ 01-SonarSweep/SonarSweep.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+"""
+#Problem 1 Project Euler Solution
+#https://projecteuler.net/problem=1
+"""
+
+__author__ = "Adam Karl"
+
+#import sys
+
+INPUT_FILE = 'input.txt'
+
+
+def SonarSweep():
+    """Parses and input file of depths and count the number of times a depth 
+    measurement increases from the previous measurement"""
+    input = list()
+    
+    #parse input file as integer list
+    with open(INPUT_FILE) as file:
+        input = [int(line) for line in file]
+    file.close()
+    
+    increases = 0
+    lastDepth = -1
+    for depth in input:
+        if depth>lastDepth and lastDepth>0:
+            increases += 1
+        lastDepth = depth
+    
+    return increases
+    
+def main():    
+    print("How many times does the depth increase?")
+    increases = SonarSweep()
+    print(f"Answer: {increases} times")
+
+if __name__ == "__main__":
+    main()
